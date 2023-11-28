05fde2900bc132f2534ee34dc9926956aa63db13
Bruno Carneiro <b.h.r.carneiro@gmail.com>
2023-11-28 12:18:12 +0000

    Base of all pages created

--- kampus/src/App.jsx
+++ kampus/src/App.jsx
@@ -2,16 +2,20 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import LandingPage from './Pages/LandingPage'
 import LoginPage from './Pages/LoginPage'
-import NavBar from './Components/NavBar';
+import ProfilePage from './Pages/ProfilePage';
+import DashboardPage from './Pages/DashboardPage';
+import ContactsPage from './Pages/ContactsPage';
 
 function App() {
 
   return (
     <div>
-    <NavBar/>
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/dashboard" element={<DashboardPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/contacts" element={<ContactsPage/>}/>
       </Routes>
     </div>
   )
--- kampus/src/Pages/LoginPage/index.jsx
+++ kampus/src/Pages/LoginPage/index.jsx
@@ -1,9 +1,44 @@
+import { useState, useContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+const API_URL = "http://localhost:5005";
+
 function LoginPage(){
-    const user = localStorage.getItem("type_user")
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [errorMessage, setErrorMessage] = useState(undefined);
+    const user = localStorage.getItem("type_user");
+    //const { storeToken, authenticateUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        const requestBody = { email, password };
+
+        axios.post(`${API_URL}/auth/login`, requestBody).then((response) => {
+
+        console.log("JWT token", response.data.authToken);
+        //storeToken(response.data.authToken);
+        //authenticateUser();
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+    }
+
     return(
         <div>
             <h1>Hi {user}</h1>
-
+            <form onSubmit={handleLogin}>
+                <label>Email <input type="email" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}></input></label>
+                <label>Password <input type="email" name="email" value={password} onChange={(e)=>{setPassword(e.target.value)}}></input></label>
+                <button type="submit">Login</button>
+            </form>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
         </div>
     )
 }
