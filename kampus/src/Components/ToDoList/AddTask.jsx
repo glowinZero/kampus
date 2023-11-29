c3f5eb4e4c49f6bbd59496693c1f8875d71b5a2f
Andre Gomes <andrefilipe.valeriogomes@gmail.com>
2023-11-29 15:37:28 +0000

    To do list updated

--- kampus/src/Components/ToDoList/AddTask.jsx
+++ kampus/src/Components/ToDoList/AddTask.jsx
@@ -2,41 +2,58 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const BACKEND_TODO_URL = "http://localhost:3000/api/tasks";
+const BACKEND_TODO_URL = "http://localhost:5005";
 
 const AddTask = () => {
   const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [body, setBody] = useState('');
+  const [deadline, setDeadline] = useState('');
+  const [status, setStatus] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Call the onSubmit function passed from the parent component
     const requestBody = {
-        title,
-        description,
-      };
-      
-    axios.post(`${BACKEND_TODO_URL}`, requestBody).then(() => {
-        navigate(`/Todolist/${taskId}`);
+      title,
+      body,
+      deadline,
+      status
+    };
+
+    axios.post(`${BACKEND_TODO_URL}/api/task`, requestBody)
+      .then((response) => {
+        const newTaskId = response.data._id;
+        localStorage.setItem("taskId", newTaskId)
+        navigate(`/Todolist/${newTaskId}`);
       })
+      .catch((error) => {
+        console.error('Error creating task:', error);
+        setErrorMessage('Failed to create task. Please try again.');
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="custom-form">
-      <label> Task:<input type="text" name="title" value={title} onChange={(e)=>setTitle(e.target.value)}/></label>
-      
-     
-      <label> Description: <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-      </label>
+    <div>
+      <form onSubmit={handleSubmit} className="custom-form">
+        <label> Task:<input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} /></label>
 
-      <button type="submit">Submit</button>
-    </form>
+        <label> Description: <input type="text" value={body} onChange={(e) => setBody(e.target.value)} /></label>
+
+        <label> Deadline: <input type="date" value={deadline} onChange={(e) => setDeadline(e.target.value)} /></label>
+
+        <label> Status: <input type="text" value={status} onChange={(e) => setStatus(e.target.value)} /></label>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {successMessage && <div style={{ color: 'green' }}>{successMessage}</div>}
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+    </div>
   );
 };
 
 export default AddTask;
-
-
--- kampus/src/Components/ToDoList/EditTask.jsx
+++ kampus/src/Components/ToDoList/EditTask.jsx
@@ -2,47 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
-const BACKEND_TODO_URL = "";
+const BACKEND_TODO_URL = "http://localhost:5005"; // Update with your backend URL
 
 const EditTask = () => {
   const { taskId } = useParams();
 
   const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-
+  const [body, setBody] = useState('');
+  const [deadline, setDeadline] = useState('');
+  const [status, setStatus] = useState('');
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Fetch book details based on the ID and populate the state
     axios.get(`${BACKEND_TODO_URL}/${taskId}`)
       .then((response) => {
-        const TaskDetails = response.data;
-        setTitle(TaskDetails.title);
-        setDescription(TaskDetails.description);
-
-
+        const taskDetails = response.data;
+        setTitle(taskDetails.title);
+        setBody(taskDetails.body);
+        setDeadline(taskDetails.deadline);
+        setStatus(taskDetails.status);
       })
       .catch((error) => {
         console.error("Error fetching Task details:", error);
       });
-  }, [id]);
+  }, [taskId]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const requestBody = {
-        title,
-        description,
-        
-      };
+      title,
+      body,
+      deadline,
+      status,
+    };
 
-    axios.put(`${BACKEND_TODO_URL}/${id}`, requestBody)
+    axios.put(`${BACKEND_TODO_URL}/${taskId}`, requestBody)
       .then(() => {
-        navigate(`/Todolist/${id}`);
+        navigate(`/Todolist/${taskId}`);
       })
       .catch((error) => {
-        console.error("Error updating book details:", error);
+        console.error("Error updating task details:", error);
       });
   };
 
@@ -64,8 +65,28 @@ const EditTask = () => {
           <input
             type="text"
             name="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Deadline:
+          <input
+            type="date"
+            name="deadline"
+            value={deadline}
+            onChange={(e) => setDeadline(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Status:
+          <input
+            type="text"
+            name="status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
           />
         </label>
 
