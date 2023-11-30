9847423c67c2756bf0eaec740bb2a6b771529789
David Lucca <david.v.lucca@gmail.com>
2023-11-30 10:26:54 +0000

    Landing page styling 80% done, missing linking with backend and more

--- kampus/src/Pages/LandingPage/index.jsx
+++ kampus/src/Pages/LandingPage/index.jsx
@@ -1,16 +1,17 @@
 import { useNavigate } from "react-router-dom"
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
-import {MailIcon} from '../../Components/MailIcon';
-import {LockIcon} from '../../Components/LockIcon';
+import {Spacer} from "@nextui-org/react";
 import React from "react";
 function LandingPage(){
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
     const navigate = useNavigate();
 
+    /*
     const isStudent = () => {
         localStorage.setItem("type_user", "student")
         navigate("/login")
     }
+    */
 
     const isStaff = () => {
         localStorage.setItem("type_user", "staff")
@@ -19,25 +20,27 @@ function LandingPage(){
 
 
     return(
-        <div id="landing-page">
-            <h1>Welcome!</h1>
-            <Button onPress={onOpen} color="secondary">STUDENT</Button>
+        <div id="landing-page" className="scale-200">
+            <h1 className="[word-spacing:-100px] text-7xl font-thin">Welcome!</h1>
+            <Spacer y={10} />
+            <div>
+            <Button onPress={onOpen} size="lg" className="bg-[#1f2d91] text-white w-64 h-12 font-medium shadow-lg">STUDENT</Button>
                 <Modal
                     classNames={{
                         size: "4xl",
-                        autoFocus: "border-blue-500",
                         body: "py-6",
                         backdrop: "bg-[#292f46]/50 backdrop-opacity-40 blur",
-                        base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
+                        base: "border-[#292f46] bg-white text-[#71717a]",
                         header: "border-b-[1px] border-[#292f46]",
                         footer: "border-t-[1px] border-[#292f46]",
-                        closeButton: "hover:bg-white/5 active:bg-white/10",
+                        closeButton: "active:bg-white/10",
                     }}
-                    size="3xl"
+                    size="l"
+                    backdrop="blur"
                     isOpen={isOpen}
                     onOpenChange={onOpenChange}
                     placement="center">
-                    <ModalContent color="secondary">
+                    <ModalContent>
                     {(onClose) => (
                         <>
                         <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
@@ -46,13 +49,13 @@ function LandingPage(){
                             autoFocus
                             label="Email"
                             placeholder="Enter your email"
-                            variant="bordered"
+                            variant="underlined"
                             />
                             <Input
                             label="Password"
                             placeholder="Enter your password"
                             type="password"
-                            variant="bordered"
+                            variant="underlined"
                             />
                             <div className="flex py-2 px-1 justify-between">
                             <Checkbox
@@ -70,7 +73,7 @@ function LandingPage(){
                             <Button color="danger" variant="flat" onPress={onClose}>
                             Close
                             </Button>
-                            <Button color="primary" onPress={onClose}>
+                            <Button color="primary" onPress={onClose} >
                             Sign in
                             </Button>
                         </ModalFooter>
@@ -78,10 +81,10 @@ function LandingPage(){
                     )}
                     </ModalContent>
                 </Modal>
-            <div style={{display: 'flex'}}>
-                <Button color="success" onClick={isStudent} isLoading>STUDENT</Button>
-                <Button onClick={isStaff}>STAFF</Button>
             </div>
+            <Spacer y={4} />
+
+                <Button onClick={isStaff} size="lg" className="w-64 bg-[#1f2d91] text-white shadow-lg">STAFF</Button>
         </div>
     )
 }
