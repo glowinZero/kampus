408709a02219fce01933286fd7919abbcc50ac5e
David Lucca <david.v.lucca@gmail.com>
2023-12-05 09:50:34 +0000

    Login page semi done

--- kampus/src/Pages/LandingPage/index.jsx
+++ kampus/src/Pages/LandingPage/index.jsx
@@ -1,116 +1,106 @@
-import { useNavigate} from "react-router-dom"
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
-import {Spacer} from "@nextui-org/react";
-import { AuthContext } from '../../Context/auth.context';
-import axios from "axios"
-import {useContext, useState} from 'react';
+import { useNavigate } from "react-router-dom";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Checkbox,
+  Input,
+  Link,
+} from "@nextui-org/react";
+import { Spacer } from "@nextui-org/react";
+import React from "react";
+function LandingPage() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const navigate = useNavigate();
 
-const API_URL = "http://localhost:5005";
-
-function LandingPage(){
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-
-    const {storeToken, authenticateUser} = useContext(AuthContext);
-    
-    const handleLoginSubmit = (e) =>{
-        e.preventDefault();
-
-        const requestBody = {email, password};
-
-        axios.post(`${API_URL}/auth/login`, requestBody)
-            .then((response)=>{
-                storeToken(response.data.authToken);
-                localStorage.setItem("LoggedIn", response.data.authToken)
-                authenticateUser();
-                navigate('/virtualtour');
-            })
-            .catch((error)=>{
-                const errorDescription = error.response.data.message; 
-                setError(errorDescription);
-            })
-    }
-
-    const isStaff = () => {
-        localStorage.setItem("type_user", "staff")
+  /*
+    const isStudent = () => {
+        localStorage.setItem("type_user", "student")
         navigate("/login")
     }
+    */
 
-    return(
-        <div id="landing-page" className="scale-200">
-            <h1 className="[word-spacing:-100px] text-7xl font-thin">Welcome!</h1>
-            <Spacer y={10} />
-            <div>
-            <Button onPress={onOpen} size="lg" className="bg-[#D3D3D3] text-[#00072D] w-64 h-12 font-semibold shadow-lg">STUDENT</Button>
-                <Modal
-                    classNames={{
-                        size: "4xl",
-                        body: "py-6",
-                        backdrop: "bg-[#292f46]/50 backdrop-opacity-40 blur",
-                        base: "border-[#292f46] bg-white text-[#71717a]",
-                        header: "border-b-[1px] border-[#292f46]",
-                        footer: "border-t-[1px] border-[#292f46]",
-                        closeButton: "active:bg-white/10",
-                    }}
-                    size="l"
-                    backdrop="blur"
-                    isOpen={isOpen}
-                    onOpenChange={onOpenChange}
-                    placement="center">
-                    <ModalContent>
-                    {(onClose) => (
-                        <>
-                        <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
-                        <ModalBody>
-                            <Input
-                            autoFocus
-                            label="Email"
-                            placeholder="Enter your email"
-                            variant="flat"
-                            value={email} 
-                            onChange={(e)=> setEmail(e.target.value)}
-                            />
-                            <Input
-                            label="Password"
-                            placeholder="Enter your password"
-                            type="password"
-                            variant="flat"
-                            value={password} 
-                            onChange={(e)=> setPassword(e.target.value)}
-                            />
-                            <div className="flex py-2 px-1 justify-between">
-                            <Checkbox
-                                classNames={{
-                                label: "text-small",
-                                }}>
-                                Remember me
-                            </Checkbox>
-                            <Link color="primary" href="#" size="sm">
-                                Forgot password?
-                            </Link>
-                            </div>
-                        </ModalBody>
-                        <ModalFooter>
-                            <Button color="danger" variant="flat" onPress={() => onClose()}>
-                                Close
-                            </Button>
-                            <Button color="primary" onClick={handleLoginSubmit} >
-                            Sign in
-                            </Button>
-                        </ModalFooter>
-                        </>
-                    )}
-                    </ModalContent>
-                </Modal>
-            </div>
-            <Spacer y={4} />
-                <Button onClick={isStaff} size="lg" className="w-64 bg-[#D3D3D3] text-[#00072D] font-semibold shadow-lg">STAFF</Button>
-                {error && <p>{error}</p>}
-        </div>
-    )
+  const isStaff = () => {
+    localStorage.setItem("type_user", "staff");
+    navigate("/login");
+  };
+
+  return (
+    <div id="landing-page" className="scale-200">
+      <h1 className="[word-spacing:-100px] text-7xl font-light">Welcome!</h1>
+      <Spacer y={10} />
+      <div>
+        <Button
+          onPress={onOpen}
+          size="lg"
+          className="bg-[#D3D3D3] text-[#00072D] w-64 h-12 font-semibold shadow-lg"
+        >
+          STUDENT
+        </Button>
+        <Modal
+          classNames={{
+            size: "4xl",
+            body: "py-6",
+            backdrop: "bg-[#292f46]/50 backdrop-opacity-40 blur",
+            base: "border-[#292f46] bg-white text-[#71717a]",
+            header: "border-b-[1px] border-[#292f46]",
+            footer: "border-t-[1px] border-[#292f46]",
+            closeButton: "active:bg-white/10",
+          }}
+          size="l"
+          backdrop="blur"
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          placement="center"
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Log in
+                </ModalHeader>
+                <ModalBody>
+                  <Input
+                    autoFocus
+                    label="Email"
+                    placeholder="Enter your email"
+                    variant="flat"
+                  />
+                  <Input
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                    variant="flat"
+                  />
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="flat" onPress={onClose}>
+                    Close
+                  </Button>
+                  <Button color="primary" onPress={onClose}>
+                    Sign in
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </div>
+      <Spacer y={4} />
+
+      <Button
+        onClick={isStaff}
+        size="lg"
+        className="w-64 bg-[#D3D3D3] text-[#00072D] font-semibold shadow-lg"
+      >
+        STAFF
+      </Button>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
